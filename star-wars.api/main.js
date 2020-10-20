1b057f63f65900914f1d7d395b0cebec8e30c1f5
Boston Misner <10922541@uvu.edu>
2020-10-20 12:37:41 -0600

    Looped Through films and people

--- star-wars.api/main.js
+++ star-wars.api/main.js
@@ -1,3 +1,19 @@
 import { films } from './Data/films.js'
+import { people } from './data/people.js'
 
-console.log(films.length)
+// console.log(document.body.children)
+
+const main = document.querySelector('main')
+
+// films.forEach(film => {
+//     console.log(film.title)
+//     let newTitle = main.appendChild(document.createElement("p"))
+//     newTitle.textContent = film.opening_crawl
+// })
+
+people.forEach(person => {
+    console.log(person.name)
+    let personImg = document.createElement('img')
+    personImg.src = "https://starwars-visualguide.com/assets/img/characters/1.jpg"
+    main.appendChild(personImg)
+})
